8c6068ba102d5fc477a4b6fc2f302dee4aece65c
arpitrohela <arpitrohela@gmail.com>
2021-07-25 15:08:59 +0530

    Update waybackurlCommunityScriptStandAlone.py

--- waybackurlCommunityScriptStandAlone.py
+++ waybackurlCommunityScriptStandAlone.py
@@ -1,13 +1,12 @@
 
 """
-
-Standalone scripts have no template.
-They are only evaluated when you run them. 
-
+Targeted scripts can only be invoked by you, the user, e.g. via a right-click option on the Sites or History tabs
 """
-#https://github.com/internetarchive/wayback/tree/master/wayback-cdx-server#filtering
-
 import urllib
-params = "example.com" #enter your domain here
-f = urllib.urlopen("http://web.archive.org/cdx/search/cdx?url="+params+"/*&output=txt&matchType=domain&limit=1000")
-print f.read()
+def invokeWith(msg):
+  # Debugging can be done using print like this
+  print('invokeWith called for url=' + msg.getRequestHeader().getURI().toString()); 
+  URL=msg.getRequestHeader().getURI().toString();
+  f = urllib.urlopen("http://web.archive.org/cdx/search/cdx?url="+URL+"/*&output=txt&matchType=domain&limit=1000&collapse=urlkey&matchType=prefix&showDupeCount=true")
+  print f.read()
+
